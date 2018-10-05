080dc7456bfc537b422b7230eb0ebc8d3f7ed407
Daniel N Somoano <dnsomoano@gmail.com>
2018-10-04 22:57:42 -0400

    Initial commit. Made first poco and db.

---
+++ server/DailyMapContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace capstone
+{
+    public partial class DailyMapContext : DbContext
+    {
+        public DailyMapContext()
+        {
+        }
+
+        public DailyMapContext(DbContextOptions<DailyMapContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("server=localhost;username=postgres;password=mark;database=DailyMap");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+
+        public DbSet<Profiles> Profiles { get; set; }
+    }
+}
---
+++ server/Models/Profiles.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace capstone
+{
+    public class Profiles
+    {
+        public int Id { get; set; }
+        public string EmailAddress { get; set; }
+        public string UserName { get; set; }
+    }
+}
