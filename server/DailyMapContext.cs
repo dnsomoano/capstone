c615660fa7087124dd2a54fc52420a710db03053
Daniel N Somoano <dnsomoano@gmail.com>
2018-10-05 00:28:57 -0400

    Added first migration to db.

--- server/DailyMapContext.cs
+++ server/DailyMapContext.cs
@@ -25,7 +25,29 @@ namespace capstone
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {}
+        {
+            modelBuilder.Entity<Profiles>().HasData(
+                new Profiles
+                {
+                    Id = 1,
+                    EmailAddress = "dnsomoano@gmail.com",
+                    UserName = "dnsomoano",
+                    Address = "2220 Central Ave.",
+                    Latitude = 27.770740,
+                    Longitude = -82.663530,
+
+                },
+                new Profiles
+                {
+                    Id = 2,
+                    EmailAddress = "mallorysmith6464@gmail.com",
+                    UserName = "Marssmith",
+                    Address = null,
+                    Latitude = 0.0000,
+                    Longitude = 0.0000,
+                }
+            );
+        }
 
         public DbSet<Profiles> Profiles { get; set; }
     }
--- server/Models/Profiles.cs
+++ server/Models/Profiles.cs
@@ -7,5 +7,8 @@ namespace capstone
         public int Id { get; set; }
         public string EmailAddress { get; set; }
         public string UserName { get; set; }
+        public string Address { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
